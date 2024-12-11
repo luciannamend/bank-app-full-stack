a04e408c03bca84bb0bfa981cd71c77af7c5f959
Lucianna Mendonca <luciannamend@gmail.com>
2024-12-11 16:12:17 -0500

    created display banks component

--- WebApp/lucianna_finaltest_comp303w/src/App.jsx
+++ WebApp/lucianna_finaltest_comp303w/src/App.jsx
@@ -1,20 +1,29 @@
-import { useState } from 'react'
-import './App.css'
+import { Routes, Route, Link } from 'react-router-dom';
+import './css/App.css'
+import DisplayBanks from "./DisplayBanks.jsx";
 
 function App() {
 
   return (
     <>
-
-      <h1>Welcome to Banks WebApp</h1>
-        <p className="technologies-applied">
-            Using:<br/>
-            - Java and Javascript (React)<br/>
-            - SpringBoot MVC<br/>
-            - Hibernate<br/>
-            - Maven<br/>
-            - MySQL <br/>
-        </p>
+        <div>
+            <nav>
+                <Link to="/banks">Display Banks</Link> |
+                {/*<Link to="/profile"> Profile</Link> |*/}
+                {/*<Link to="/history"> History</Link> |*/}
+                {/*<Link to="/registration"> Registration</Link> |*/}
+                {/*<Link to="/login"> Login</Link>*/}
+                {/*<Link to="/logout">Logout</Link>*/}
+            </nav>
+            <Routes>
+                <Route path="/banks" element={<DisplayBanks/>}/>
+                {/*<Route path="/profile" element={<DonorProfile/>}/>*/}
+                {/*<Route path="/history" element={<DonorHistory/>}/>*/}
+                {/*<Route path="/registration" element={<Registration/>}/>*/}
+                {/*<Route path="/login" element={<Login/>}/>*/}
+                {/*<Route path="/logout" element={<Logout/>}/>*/}
+            </Routes>
+        </div>
     </>
   )
 }
