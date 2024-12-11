63af3ce52beb4cbb09401c8e4dbc575b42c3d04e
Lucianna Mendonca <luciannamend@gmail.com>
2024-12-10 20:33:38 -0500

    Initial commit

---
+++ API/Lucianna_FinalTest_COMP303a/src/main/java/com/lucianna/mendonca/lucianna_finaltest_comp303a/LuciannaFinalTestComp303aApplication.java
@@ -0,0 +1,13 @@
+package com.lucianna.mendonca.lucianna_finaltest_comp303a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuciannaFinalTestComp303aApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LuciannaFinalTestComp303aApplication.class, args);
+    }
+
+}
---
+++ API/Lucianna_FinalTest_COMP303a/src/test/java/com/lucianna/mendonca/lucianna_finaltest_comp303a/LuciannaFinalTestComp303aApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucianna.mendonca.lucianna_finaltest_comp303a;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuciannaFinalTestComp303aApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
