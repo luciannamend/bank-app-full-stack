61ea776723761826b05d8ef16a04d06a269a20d1
Lucianna Mendonca <luciannamend@gmail.com>
2024-12-12 22:25:34 -0500

    Fixed style details

--- WebApp/lucianna_finaltest_comp303w/src/components/BankList.jsx
+++ WebApp/lucianna_finaltest_comp303w/src/components/BankList.jsx
@@ -1,11 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import  { useState, useEffect } from 'react';
 import { getAllBanks } from '../services/bankService.js';
 import '../css/App.css'
 
 function BankList() {
     const [banks, setBanks] = useState([]);
-    // const [bankId, setBankId] = useState('');
-    // const [bankName, setBankName] = useState('');
 
     useEffect(() => {
         getAllBanks()
@@ -24,7 +22,8 @@ function BankList() {
                 {banks.map((bank) => (
                     <li key={bank.bankId}>
                         <p>Bank Name:<strong> {bank.bankName}</strong></p>
-                        <p>Founded in {bank.bankYear}, has {bank.bankEmp} employees, {bank.bankBranches} branches, and {bank.bankATMs} ATMs</p>
+                        <p>Founded in {bank.bankYear}, has {bank.bankEmp} employees, {bank.bankBranches} branches,
+                            and {bank.bankATMs} ATMs.</p>
                         <p>Address: {bank.bankAddress}</p>
                     </li>
                 ))}
