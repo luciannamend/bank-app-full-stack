003d32f01c4559648fc6d6d1f918a93cf6944011
Lucianna Mendonca <luciannamend@gmail.com>
2024-12-13 16:34:17 -0500

    changed nav

--- WebApp/lucianna_finaltest_comp303w/src/App.jsx
+++ WebApp/lucianna_finaltest_comp303w/src/App.jsx
@@ -11,20 +11,16 @@ function App() {
     <>
         <div>
             <nav>
-                <Link to="/banks"> Bank List </Link> |
-                <Link to="/"> Create </Link> |
-                <Link to="/update-bank/id"> Manage By Id </Link> |
-                <Link to="/update-bank/name"> Manage By Name</Link> |
-                {/*<Link to="/login"> Login</Link>*/}
-                {/*<Link to="/logout">Logout</Link>*/}
+                <Link to="/banks"> Bank List </Link> ||
+                <Link to="/"> Create </Link> ||
+                <Link to="/update-bank/id"> Manage By Id </Link> ||
+                <Link to="/update-bank/name"> Manage By Name</Link>
             </nav>
             <Routes>
                 <Route path="/banks" element={<BankList/>}/>
                 <Route path="/" element={<CreateBank/>}/>
                 <Route path="/update-bank/id" element={<ManageBankById/>}/>
                 <Route path="/update-bank/name" element={<ManageBankByName/>}/>
-                {/*<Route path="/login" element={<Login/>}/>*/}
-                {/*<Route path="/logout" element={<Logout/>}/>*/}
             </Routes>
         </div>
 
